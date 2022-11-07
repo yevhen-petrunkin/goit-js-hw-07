26cee76be342acf7b5e6d42a7f5c52bf9d76da27
yevhen-petrunkin <novamovaxxi@gmail.com>
2022-11-07 23:07:24 +0200

    Create gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let galleryMarkup = '';
+const galleryRef = document.querySelector('.gallery');
+
+createGalleryMarkup(galleryItems);
+
+createGalleryFromMarkup(galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return (galleryMarkup = items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(''));
+}
+
+function createGalleryFromMarkup(markup) {
+  galleryRef.innerHTML = markup;
+}
